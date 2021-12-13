d1c60ac1b2dd9ff58d0d2b4843d14099cc17c1a0
BOUZID <aminebouzid2014@gmail.com>
2021-12-13 18:55:03 +0100

    assign manager to user

--- front/src/app/register/register.component.ts
+++ front/src/app/register/register.component.ts
@@ -1,6 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { UserService } from '../_services/user.service';
 
+interface User {
+  id: Number;
+  username: String;
+  email: String;
+  password: String;
+  roles: [];
+  nom: String;
+  prenom: String;
+  manager: any;
+}
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -9,23 +20,38 @@ import { AuthService } from '../_services/auth.service';
 export class RegisterComponent implements OnInit {
 
   form: any = {
+    nom: null,
     username: null,
     email: null,
-    password: null
+    password: null,
+    manager: null,
+    prenom: null
   };
   isSuccessful = false;
   isSignUpFailed = false;
   errorMessage = '';
+  managerNotAdded = false;
+  managers: User[] = [];
+  selectedManager: string = "";
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private userService: UserService) { }
 
   ngOnInit(): void {
+    this.userService.getAllManagers().subscribe(
+      data => {
+        this.managers = data;
+      },
+    )
   }
 
   onSubmit(): void {
-    const { username, email, password } = this.form;
+    const { nom, prenom, username, email, password, manager } = this.form;
 
-    this.authService.register(username, email, password).subscribe(
+    if (manager == null) {
+      this.managerNotAdded = true;
+    }
+    console.log(manager);
+    this.authService.register(username, email, password, manager, nom, prenom).subscribe(
       data => {
         console.log(data);
         this.isSuccessful = true;
